22fa9854346f62454eb8fc5fbc7cfc21fca353ca
Emil P Lundh <33914352+emilpedersenlundh@users.noreply.github.com>
2023-02-22 16:00:39 +0100

    main file pseudo

---
+++ app/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from app.radio.radio import Radio
+from app.server.server import Server
+
+
+def main():
+    pass
+    # TODO:
+    # Start server (which starts an interface)
+    # Start radio
+    #
+    # If mode is base:
+    # Listen on radio until data is received
+    # Write data to server
+    # Wait for response from server
+    # Transmit response through radio
+    #
+    # If mode is node:
+    # Read server until non-zero data is retrieved
+    # Transmit data through radio
+    # Listen on radio until data is received
+    # Write data to server
+
+
+if __name__ == "__main__":
+    main()
